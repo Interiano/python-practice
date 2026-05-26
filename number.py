b0cc9dedaf100a28a689a72e52828fd8c4d3ff55
Julio Interiano <interiano88.julio@gmail.com>
2026-05-26 02:50:58 -0700

    Day 97 - exceptions and error handling

--- number.py
+++ number.py
@@ -1,2 +1,12 @@
-x = int(input("what's x? "))
-print(f"x is {x}")
+def main():
+    x = get_int("What's x? ")
+    print(f"x is {x}")
+
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            pass
+
+main()
